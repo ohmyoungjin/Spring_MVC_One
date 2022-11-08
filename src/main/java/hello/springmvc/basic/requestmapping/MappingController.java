4265e6904f7e41cab56222ac1d833361d68cc06e
ohmyoungjin <sisaudwls@naver.com>
2022-11-08 10:08:16 +0900

    20221108 :: Spring MVC RequestMapping PathVariable

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,8 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,37 @@ public class MappingController {
     @RequestMapping("/hello-basic")
     public String helloBasci() {
         log.info("hellobasic");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     * 변수명이 같은 경우
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     */
+    @GetMapping("mapping/single/{userId}")
+    public String mappingPath2(@PathVariable String userId) {
+        log.info("mappingPath userId={}", userId);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingmultiPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
         return "ok";
     }
 }
